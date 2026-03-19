ad0047f535221964190467a5c2d32386408e03f9
MiguelGrilo <miguelgrilo82@gmail.com>
2026-03-19 21:00:02 +0000

    Update DotFiles: 2026-03-19 21:00

--- fish/.config/fish/functions/fastfetch.fish
+++ fish/.config/fish/functions/fastfetch.fish
@@ -1,3 +1,3 @@
-function fastfetch --description 'alias fastfetch=fastfetch --logo-color-1 blue --logo-color-2 blue --color-keys magenta'
-    command fastfetch --logo-color-1 blue --logo-color-2 blue --color-keys magenta $argv
+function fastfetch --wraps=fastfetch2 --description 'alias fastfetch=fastfetch2'
+    fastfetch2 $argv
 end
